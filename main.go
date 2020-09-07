dc6f8f8debb144806090dbf41211ba69b237d517
hcuri <hcuri@riotgames.com>
2020-09-06 20:18:19 -0700

    Test modules with cmp

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Println("hello, world.")
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+
 }
